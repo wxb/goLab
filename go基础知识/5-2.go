21edf0e2f7548bbabec7eac19aeff2c999246fb5
wxb <wxb0328@gmail.com>
2016-07-31 02:33:56 +0800

    go切片类型

--- go基础知识/5-2.go
+++ go基础知识/5-2.go
@@ -7,8 +7,9 @@ func main() {
 	//var slice = number[1:4]
 	//fmt.Printf("%d - %d - %v ", number[4], slice[2], 4==cap(slice))
 
-	var numbers3 = [5]int{1, 2, 3, 4, 5}
-	slice3 := numbers3[2:len(numbers3)]
+	var numbers3 = [6]int{1, 2, 3, 4, 5, 6}
+	//slice3 := numbers3[2:len(numbers3)] //[2,5)
+	slice3 := numbers3[2:5:5] //[2,5)
 	length := (3)
 	capacity := (3)
 	fmt.Printf("%v, %v\n", (length == len(slice3)), (capacity == cap(slice3)))
