7700e773aec97f6ffb63b0e1ee8f07837b803cf6
wxb <wxb0328@icloud.com>
2018-04-17 10:33:55 +0800

    Add + ccmouse教程笔记

--- ccmouse/helloworld/index.go
+++ ccmouse/helloworld/index.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintln(writer, "<h1>Hello, World!</h1>")
-	})
-
-	http.ListenAndServe(":8888", nil)
+	fmt.Println("Hello, World!")
 }
