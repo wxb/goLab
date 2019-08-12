ed736ec4bce79bea4440d1449104871e069c393e
虢国技酱 <wxb0328@icloud.com>
2019-08-12 09:34:12 +0800

    feat(caichao): context

---
+++ geek/goCaiChao/ch29/cancel_by_context_test.go
@@ -0,0 +1,46 @@
+package ch29_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func isCancelled(ctx context.Context) bool {
+	select {
+	case <-ctx.Done():
+		return true
+	default:
+		return false
+	}
+}
+
+func cancel1(ch chan struct{}) {
+	ch <- struct{}{}
+}
+
+func cancel2(ch chan struct{}) {
+	close(ch)
+}
+
+func TestCancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	for i := 0; i < 30; i++ {
+		go func(i int) {
+			for {
+				if isCancelled(ctx) {
+					break
+				} else {
+					// fmt.Println(i, "Not Yet")
+				}
+				time.Sleep(time.Millisecond * 5)
+			}
+			fmt.Println(i, "Canceled")
+		}(i)
+	}
+
+	cancel()
+	time.Sleep(time.Millisecond * 100)
+}
