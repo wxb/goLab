93a68091b56b7ac6d69e61a7850873787dfa3f53
wxb <wxb0328@icloud.com>
2019-09-21 22:18:19 +0800

    feat(map): 学习map特性

--- geek/goCaiChao/ch10/map_test.go
+++ geek/goCaiChao/ch10/map_test.go
@@ -1,1 +1,47 @@
 package ch10_test
+
+import "testing"
+
+func TestInitMap(t *testing.T) {
+	m1 := map[string]int{"one": 1, "two": 2}
+
+	m2 := map[string]int{}
+	m2["three"] = 3
+
+	m3 := make(map[string]int, 10) // 10 is cap, not len; 这里如果我们可以确定容量 有利于提高性能
+	m3["four"] = 4
+
+	var m4 map[string]int
+	// m4["five"] = 5       // panic: assignment to entry in nil map
+	m4 = map[string]int{"five": 5}
+
+	t.Log(m1, m2, m3, m4)
+	t.Log(len(m1), len(m2), len(m3), len(m4))
+	// t.Log(cap(m1), cap(m2), cap(m3), cap(m4))
+}
+
+func TestAccessNotExistingKey(t *testing.T) {
+	m1 := map[int]int{}
+
+	// 在指定key不存在时，go并不会异常或者错误，而会返回该map值的类型零值
+	t.Log(m1[1])
+
+	m1[2] = 0
+	t.Log(m1[2])
+
+	// 利用这种形式来确认，map中是否存在某个key值
+	if v, ok := m1[3]; ok {
+		t.Log("key 3's value is ", v)
+	} else {
+		t.Log("key 3 is not exist")
+	}
+}
+
+func TestTravelMap(t *testing.T) {
+	m := map[int]string{1: "one", 2: "two", 3: "three"}
+	// 利用for-range可以遍历出map的键值对
+	for k, v := range m {
+		t.Log(k, v)
+	}
+	// 输出键值对顺序，map底层使用的是hash存储，不是有序的
+}
--- geek/goCaiChao/ch11/map_test.go
+++ geek/goCaiChao/ch11/map_test.go
@@ -1,1 +1,74 @@
 package ch11_test
+
+import (
+	"testing"
+)
+
+// map与工厂模式
+
+// 函数在go里面是一等公民
+// map的值可以是一个函数或者说方法
+//
+
+type f1 func(i int) int
+
+func TestMapWithFunValue(t *testing.T) {
+	m := map[int]f1{}
+	m[1] = func(i int) int { return i }
+	m[2] = func(ii int) int { return ii * ii }
+	m[3] = func(iii int) int { return iii * iii * iii }
+
+	t.Log(m[1](2), m[2](2), m[3](2))
+}
+
+// 实现Set(一组key的集合,key不能重复;所以，在set中，没有重复的key。)
+// go内置没有实现set,可以利用 map[type]bool 来实现
+// 1. 元素的唯一性
+// 2. 基本操作：a.添加元素；b.删除元素；c.判断元素是否存在；d.返回元素个数
+
+// type set struct {
+// 	t reflect.Kind
+// 	m map[reflect.Kind]bool
+// }
+
+// func NewSet(list []interface{}) {
+// 	s := set{}
+// 	for _, v := range list {
+// 		t := reflect.TypeOf(v).Kind()
+
+// 	}
+// }
+
+// func Add() {
+
+// }
+
+// func Del() {
+
+// }
+
+// func Num() {
+
+// }
+
+func TestMapForSet(t *testing.T) {
+	mySet := map[int]bool{}
+	mySet[1] = true
+	n := 3
+	if mySet[n] {
+		t.Log(n, "is existing")
+	} else {
+		t.Log(n, "is not existing")
+	}
+
+	mySet[3] = true
+	t.Log(mySet)
+
+	delete(mySet, 1)
+	if mySet[n] {
+		t.Log(n, "is existing")
+	} else {
+		t.Log(n, "is not existing")
+	}
+
+}
