3ac9b74ac625ddc9ee0be3638285c7e60b5def09
虢国技酱 <wxb0328@icloud.com>
2019-12-09 17:51:30 +0800

    feat(chai2010): ch014

--- chai2010/ch01/ch014/main.go
+++ chai2010/ch01/ch014/main.go
@@ -1,26 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
+// File 文件对象
 type File struct {
-	abc string
+	Path string
 }
 
-// 关闭文件
+// Close 关闭文件
 func (f *File) Close() error {
-	// ...
-	fmt.Println(f.abc)
+	defer fmt.Println("Close: ", f.Path)
+
 	return nil
 }
 
-// 读文件数据
+// Read 读文件数据
 func (f *File) Read(offset int64, data []byte) int {
-	// ...
+	defer fmt.Println("Read: ", f.Path)
 
-	return 0
+	return len(data)
+}
+
+func (f *File) Write(data []byte) error {
+	if len(data) > 1024 {
+		return errors.New("Out of write range 1024")
+	}
+
+	return nil
+}
+
+// OpenFile 打开文件
+func OpenFile(name string) (f *File, err error) {
+	return &File{Path: name}, nil
 }
 
 func main() {
+	// f, _ := OpenFile("/path/prod.ini")
+	// f.Read(1, []byte("abc"))
+	// f.Close()
+
 	var CloseFile = (*File).Close
 
 	// 不依赖具体的文件对象
@@ -28,11 +49,12 @@ func main() {
 	var ReadFile = (*File).Read
 
 	// 文件处理
-	// f, _ := OpenFile("foo.dat")
+	f, _ := OpenFile("/path/prod.ini")
+	ReadFile(f, 0, []byte("abc"))
+	CloseFile(f)
 
 	// 通过叫方法表达式的特性可以将方法还原为普通类型的函数：
-	f := File{"123"}
-	ReadFile(&f, 0, []byte("abc"))
-	CloseFile(&f)
-
+	ff := File{"/path/test.ini"}
+	ReadFile(&ff, 0, []byte("aabbcc"))
+	CloseFile(&ff)
 }
